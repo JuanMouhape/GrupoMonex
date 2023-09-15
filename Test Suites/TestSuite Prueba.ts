91ab92a4dedc553e8327274b7944cf27e4d855dd
unknown <juancmouhape@gmail.com>
2023-09-15 18:33:09 -0300

    Se sube TestSuite

---
+++ Test Suites/TestSuite Prueba.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite Prueba</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c63c1277-575e-4737-aff8-4db236599725</testSuiteGuid>
+   <testCaseLink>
+      <guid>21969b72-a43a-4388-8433-cbe1f0a4c485</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Prueba Grupo Monex</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
